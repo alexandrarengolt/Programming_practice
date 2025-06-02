cbec1eab6af4e380c3aa566a881bb80e335b7ba1
alexandrarengolt <alikrengolt@gmail.com>
2025-06-02 10:58:29 +0600

    Revert "feat: linked list realisation"
    This reverts commit 89cb4b7cc7cf4d6f6312eb9f121d858ddb8f113a.

--- lab1/linked_list.h
+++ lab1/linked_list.h
@@ -1,16 +1,1 @@
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-typedef struct Item {
-  int current;
-  struct Item *next;
-} Item;
-
-Item *create_item(int data);
-void push_start(Item **start_elem, int data);
-void push_end(Item **start_elem, int data);
-void push_middle(Item **start_elem, int data, int index);
-void delete_item(Item **start_elem, int value);
-Item *find_item(Item *start_elem, int index);
-int count_elem(Item *start_elem);
+// ...

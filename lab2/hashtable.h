01dc503a7589bd011fe6c6df40627dba52ee4882
alexandrarengolt <alikrengolt@gmail.com>
2025-06-06 23:32:18 +0600

    feat: add hashtable realisation

--- lab2/hashtable.h
+++ lab2/hashtable.h
@@ -1,1 +1,22 @@
-// ...
+#define HASHTABLE_H
+#include "pool_allocator.h"
+
+typedef struct {
+  char *key;
+  void *value;
+  int is_occupied;
+} HashtableItem;
+
+typedef struct {
+  HashtableItem *items;
+  size_t capacity;
+  size_t value_size;
+  PoolAllocator *allocator;
+} Hashtable;
+
+void hashtable_init(Hashtable *table, size_t capacity, size_t value_size,
+                    PoolAllocator *allocator);
+int hashtable_insert(Hashtable *table, const char *key, void *value);
+void *hashtable_get(Hashtable *table, const char *key);
+void hashtable_delete(Hashtable *table, const char *key);
+void hashtable_cleanup(Hashtable *table);

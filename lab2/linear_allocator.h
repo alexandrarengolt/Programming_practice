92ad53967bad65e1718277695351affeda0c2f0e
LeeAnna241 <anastasiakisner6@gmail.com>
2025-06-06 23:46:05 +0600

    feat: add linear allocator

--- lab2/linear_allocator.h
+++ lab2/linear_allocator.h
@@ -1,1 +1,18 @@
-// ...
+#ifndef LINEAR_ALLOC_H
+#define LINEAR_ALLOC_H
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+  void *memory_block;
+  size_t total_size;
+  size_t offset;
+} linear_allocator;
+
+linear_allocator *linear_allocator_init(size_t size);
+void linear_allocator_deinit(linear_allocator *allocator);
+void *linear_allocator_alloc(linear_allocator *allocator, size_t size);
+void linear_allocator_reset(linear_allocator *allocator);
+
+#endif
